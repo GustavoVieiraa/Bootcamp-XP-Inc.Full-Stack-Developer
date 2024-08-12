7cab9dadf7921af66793c21a9d9243651dbfab7a
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-12 08:14:10 -0300

    ModificadoresDeAcesso - concluído com êxito.
    ModificadoresDeAcesso - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Propriedades, Métodos e Construtores com C#/ModificadoresDeAcesso/Program.cs
@@ -0,0 +1,10 @@
+﻿using PropriedadesNaPratica.Models;
+
+Pessoa gustavo = new Pessoa();
+
+gustavo.Nome = "Gustavo";
+gustavo.Idade = 20;
+
+gustavo.Apresentar();
+
+// gustavo.Apresentar("Gustavo", 21);
