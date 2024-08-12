8d69653353d522eef4efe0e1f7987f43e1a63d64
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-12 07:30:11 -0300

    Propriedades na Pratica - concluído com êxito.
     Propriedades na Pratica - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Propriedades, Métodos e Construtores com C#/PropriedadesNaPratica/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropriedadesNaPratica.Models
+{
+    public class Pessoa
+    {
+        
+        public string Nome { get; set; }
+        public int Idade { get; set; }        
+        public string Cpf { get; set; }
+
+    }
+}
