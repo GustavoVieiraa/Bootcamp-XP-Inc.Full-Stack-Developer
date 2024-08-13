6226efb7e6f9411f238cebd9d1fd7a659090aa41
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-13 07:48:37 -0300

    AlterandoLocalizacaoCultureInfo - concluído com êxito.
    AlterandoLocalizacaoCultureInfo - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Manipulando Valores com C#/AlterandoLocalizacaoCultureInfo/Program.cs
@@ -0,0 +1,8 @@
+﻿using Manipulando_Valores_com_C_.Models;
+using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+decimal valorMonetario = 1782.40M;
+
+Console.WriteLine($"{valorMonetario:C}");
