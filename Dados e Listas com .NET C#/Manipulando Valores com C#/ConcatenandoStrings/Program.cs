2fdca578ab3db1711448b48b21b9693e96dcf0df
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-13 07:29:19 -0300

    ConcatenandoStrings - concluído com êxito.
    ConcatenandoStrings - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Manipulando Valores com C#/ConcatenandoStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using Manipulando_Valores_com_C_.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa aluno1 = new Pessoa("Gustavo", 21);
+        Pessoa aluno2 = new Pessoa("Gabriel", 32);
+        Curso cursoJava = new Curso();
+
+        aluno1.Apresentar();
+        cursoJava.AdicionarAluno(aluno1);
+        cursoJava.AdicionarAluno(aluno2);
+        cursoJava.ListarAlunos();
+    }
+}
