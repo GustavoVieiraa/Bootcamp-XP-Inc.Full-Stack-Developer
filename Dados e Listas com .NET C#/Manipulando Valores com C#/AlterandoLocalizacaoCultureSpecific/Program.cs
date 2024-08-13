e0fe2f2fe7b7c9bac23a096a59556e72b28050e3
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-13 07:52:53 -0300

    AlterandoLocalizacaoCultureSpecific - concluído com êxito.
    AlterandoLocalizacaoCultureSpecific - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Manipulando Valores com C#/AlterandoLocalizacaoCultureSpecific/Program.cs
@@ -0,0 +1,9 @@
+﻿using Manipulando_Valores_com_C_.Models;
+using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+decimal valorMonetario = 1782.40M;
+
+Console.WriteLine($"{valorMonetario:C}");
+Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
