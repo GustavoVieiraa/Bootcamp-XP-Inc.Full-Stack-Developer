8aa1ba86d130a48ba5cc02634e1bc058e61f7786
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-13 08:18:39 -0300

    DateTimeComTryParse - concluído com êxito.
    DateTimeComTryParse - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Manipulando Valores com C#/DateTimeComTryParse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+string dataString = "13-08-2025 15:00";
+
+DateTime.TryParseExact(dataString, 
+"dd-MM-yyyy HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date);
+
+Console.WriteLine(date);
+Console.WriteLine(date.ToString("dd/MM/yyyy"));
+Console.WriteLine(date.ToString("dd/MM/yyyy HH:mm"));
+
+Console.WriteLine(date.ToString("MM/dd/yyyy"));
