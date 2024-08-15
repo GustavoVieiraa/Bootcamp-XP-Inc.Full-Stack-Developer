d9a54042c093010afb8575a9fa47238ce6348ae3
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-15 09:38:19 -0300

    BlocoFinally - concluído com êxito.
    BlocoFinally - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Exceções e Coleções com C#/BlocoFinally/Program.cs
@@ -0,0 +1,27 @@
+﻿
+try {
+
+    string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt"); // forced error
+
+    foreach (string linha in linhas) {
+
+        Console.WriteLine(linha);
+    
+    }
+    
+} catch(FileNotFoundException ex) {
+
+    Console.WriteLine($"[Arquivo não encontrado] Ocorreu um erro na leitura do arquivo. {ex.Message}");
+}
+catch(DirectoryNotFoundException ex) {
+
+    Console.WriteLine($"[Diretorio não encontrado] Ocorreu um erro na leitura do diretorio. {ex.Message}");
+}
+catch(Exception ex) {
+
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
+finally {
+    Console.WriteLine("Exceções tratadas.");
+    // normalmente utilizado para fechar comunicações, etc.
+}
