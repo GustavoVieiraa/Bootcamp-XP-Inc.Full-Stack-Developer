bafa605c4e905f083e12f8a8ba2751f2a1ba93de
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-15 09:21:02 -0300

    TratandoUmaExceção - concluído com êxito.
    TratandoUmaExceção - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Exceções e Coleções com C#/TratandoUmaExceção/Program.cs
@@ -0,0 +1,16 @@
+﻿
+try {
+
+    string[] linhas = File.ReadAllLines("Arqvos/arquivoLeitura.txt"); // forced error
+
+    foreach (string linha in linhas) {
+
+        Console.WriteLine(linha);
+    
+    }
+    
+} catch(Exception ex) {
+
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+
+}
