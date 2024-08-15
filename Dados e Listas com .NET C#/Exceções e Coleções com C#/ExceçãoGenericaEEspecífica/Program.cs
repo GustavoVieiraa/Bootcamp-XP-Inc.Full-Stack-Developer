c0560ea7917c51760746602438e6d734293ca24d
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-15 09:34:26 -0300

    ExceçãoGenericaEEspecífica - concluída com êxito.
    ExceçãoGenericaEEspecífica - concluída com êxito.

---
+++ Dados e Listas com .NET C#/Exceções e Coleções com C#/ExceçãoGenericaEEspecífica/Program.cs
@@ -0,0 +1,26 @@
+﻿
+try {
+
+    string[] linhas = File.ReadAllLines("Aruivos/arquivoLeitura.txt"); // forced error
+
+    foreach (string linha in linhas) {
+
+        Console.WriteLine(linha);
+    
+    }
+    
+} catch(FileNotFoundException ex) {
+
+    Console.WriteLine($"[Arquivo não encontrado] Ocorreu um erro na leitura do arquivo. {ex.Message}");
+
+}
+catch(DirectoryNotFoundException ex) {
+
+    Console.WriteLine($"[Diretorio não encontrado] Ocorreu um erro na leitura do diretorio. {ex.Message}");
+
+}
+catch(Exception ex) {
+
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+
+}
