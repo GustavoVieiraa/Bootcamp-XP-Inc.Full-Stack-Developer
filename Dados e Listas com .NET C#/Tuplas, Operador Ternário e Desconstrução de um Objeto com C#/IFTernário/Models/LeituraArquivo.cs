474a09b93fb8495c78fb3baf2b9ec8d1d319755b
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-16 09:50:58 -0300

    IFTernário - concluído com êxito.
    IFTernário - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Tuplas, Operador Ternário e Desconstrução de um Objeto com C#/IFTernário/Models/LeituraArquivo.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TuplasNaPrática.Models
+{
+    public class LeituraArquivo
+    {
+        public (bool Sucesso, string[] Linhas, int QuantidadeLinhas) LerArquivo(string caminho) {
+
+            try{ 
+                string[] linhas = File.ReadAllLines(caminho);
+                return (true, linhas, linhas.Count());
+            } catch (Exception) {
+                return (false, new string[0], 0);
+            }
+            
+
+        }
+
+
+    }
+}
---
+++ Dados e Listas com .NET C#/Tuplas, Operador Ternário e Desconstrução de um Objeto com C#/IFTernário/Program.cs
@@ -0,0 +1,14 @@
+﻿int numero = 16;
+bool ehPar = false;
+
+// IF Ternário
+ehPar = numero % 2 == 0;
+
+System.Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "ímpar"));
+
+// if (numero % 2 == 0) {
+//     Console.WriteLine($"O número {numero} é par!");
+// }
+// else {
+//     Console.WriteLine($"O número {numero} é impar");
+// }
