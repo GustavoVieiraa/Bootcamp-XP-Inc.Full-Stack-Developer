b7f4329d5475bca080d9f61c7935847ce7148d2d
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-16 07:59:03 -0300

    DictionaryNaPrática - concluído com êxito.
    DictionaryNaPrática - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Exceções e Coleções com C#/DictionaryNaPrática/Program.cs
@@ -0,0 +1,21 @@
+﻿using UsandoThrow.Models;
+
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+estados.Add("RJ", "Rio de Janeiro");
+
+foreach(var estado in estados) {
+    Console.WriteLine($"Chave {estado.Key} | Valor: {estado.Value}");
+}
+
+estados.Remove("MG");
+estados["SP"] = "São Paulo - Valor Diferenciado";
+
+Console.WriteLine("=-=-=-=-=-=-=-=-=");
+
+foreach(var estado in estados) {
+    Console.WriteLine($"Chave {estado.Key} | Valor: {estado.Value}");
+}
