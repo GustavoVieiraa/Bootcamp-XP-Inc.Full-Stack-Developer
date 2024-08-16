99b193aeadbad0fecbe7c42b45fdfb7029b8ee6f
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-16 07:49:30 -0300

    PilhaNaPrática - concluído com êxito.
    PilhaNaPrática - concluído com êxito.

---
+++ Dados e Listas com .NET C#/Exceções e Coleções com C#/PilhaNaPrática/Program.cs
@@ -0,0 +1,20 @@
+﻿using UsandoThrow.Models;
+
+// Pilhas utiliza o padrão LIFO (First In, Last Out)
+
+Stack<int> pilha = new Stack<int>();
+
+pilha.Push(5);
+pilha.Push(12);
+pilha.Push(7);
+pilha.Push(10);
+
+foreach (int i in pilha) {
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("Removendo o elemento: " + pilha.Pop());
+
+foreach (int i in pilha) {
+    Console.WriteLine(i);
+}
