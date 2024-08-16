8f43fe7eb71fbc73788a581d1238d7571a748ffe
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-16 09:44:02 -0300

    TuplasNaPrática - concluída com êxito.
    TuplasNaPrática - concluída com êxito.

---
+++ Dados e Listas com .NET C#/Tuplas, Operador Ternário e Desconstrução de um Objeto com C#/TuplasNaPrática/Program.cs
@@ -0,0 +1,23 @@
+﻿// (int, string, string, decimal) tuplaExemplo = (1, "Gustavo", "Vieira", 1.76M);
+
+// Console.WriteLine("ID: " + tuplaExemplo.Item1);
+// Console.WriteLine("Nome: " + tuplaExemplo.Item2);
+// Console.WriteLine("Sobrenome: " + tuplaExemplo.Item3);
+// Console.WriteLine("Altura: " + tuplaExemplo.Item4);
+
+using TuplasNaPrática.Models;
+
+LeituraArquivo arquivo = new LeituraArquivo();
+
+
+// Conceito de Descartes em Tuplas.
+var (sucesso, linhasArquivo, _) = arquivo.LerArquivo("Arquivos/testeDeLeitura.txt");
+
+if (sucesso) {
+    //Console.WriteLine("Qnt de linhas: " + quantidadeLinhas);
+    foreach (string linha in linhasArquivo) {
+        Console.WriteLine(linha);
+    }
+} else {
+    Console.WriteLine("Não foi possível ler o arquivo.");
+}
